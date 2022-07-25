f2a964af4885a81afe0ac3df0d78a7fe078bbda9
Mahammad Afnan <muhammedafnan8184@gmail.com>
2022-07-25 21:36:53 +0530

    alert dialog is ready to be used.

--- app/src/main/java/com/mubasshir/adminhomenew/Home_Fragment.java
+++ app/src/main/java/com/mubasshir/adminhomenew/Home_Fragment.java
@@ -1,39 +1,30 @@
 package com.mubasshir.adminhomenew;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.auth.User;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-
-import java.util.ArrayList;
 
 
 public class Home_Fragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-    myadapter madapter;
+    //    myadapter madapter;
     private EditText pr_name, pr_address, pr_city;
-    private Button create_project,addproject;
+    private TextView closeButton;
+    private Button create_project, addproject;
     DatabaseReference databaseProjects;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -43,28 +34,27 @@ public class Home_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_home, container, false);
-        View dialogLayout = inflater.inflate(R.layout.create_dialog, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
         // Inflate the layout for this fragment
-        addproject=(Button)view.findViewById(R.id.btn_add_project);
-        create_project=(Button)dialogLayout.findViewById(R.id.btn_create);
+        addproject = (Button) view.findViewById(R.id.btn_add_project);
 
 
-        mRecyclerView=(RecyclerView) view.findViewById(R.id.list_projects);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.list_projects);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        FirebaseRecyclerOptions<model> Options=new FirebaseRecyclerOptions.Builder<model>()
-                .setQuery(FirebaseDatabase.getInstance().getReference().child("Projects"),model.class)
-                .build();
-
-        madapter=new myadapter(Options);
+//        FirebaseRecyclerOptions<model> Options = new FirebaseRecyclerOptions.Builder<model>()
+//                .setQuery(FirebaseDatabase.getInstance().getReference().child("Projects"), model.class)
+//                .build();
+//
+//        madapter = new myadapter(Options);
         mRecyclerView.setAdapter(mAdapter);
 
 
         addproject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showdialog(dialogLayout);
+                showDialog();
             }
         });
 
@@ -72,48 +62,90 @@ public class Home_Fragment extends Fragment {
 
     }
 
-    @Override
-    public void onStart() {
-       super.onStart();
-       madapter.startListening();
-    }
+
+    private void showDialog() {
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.create_project_dialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(dialogView);
 
 
-    @Override
-   public void onStop() {
-        super.onStop();
-        madapter.stopListening();
-   }
+        pr_name = (EditText) dialogView.findViewById(R.id.p_name);
+        pr_address = (EditText) dialogView.findViewById(R.id.p_address);
+        pr_city = (EditText) dialogView.findViewById(R.id.p_city);
+        create_project = dialogView.findViewById(R.id.btn_create);
+        closeButton = dialogView.findViewById(R.id.btn_close);
 
-    private void showdialog(View dialogLayout) {
-        Dialog dialog = new Dialog(requireActivity());
-        dialog.setCancelable(true);
+        databaseProjects = FirebaseDatabase.getInstance().getReference("Admin");
 
-        dialog.setContentView(R.layout.create_dialog);
-        dialog.show();//Controlling width and height.
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(false);
+        alertDialog.show();
 
-        pr_name=(EditText) dialogLayout.findViewById(R.id.p_name);
-        pr_address=(EditText) dialogLayout.findViewById(R.id.p_address);
-        pr_city=(EditText) dialogLayout.findViewById(R.id.p_city);
-        databaseProjects= FirebaseDatabase.getInstance().getReference("Admin");
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
 
         create_project.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createdata();
-                Toast.makeText(getContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+//                createdata();
+
+                if (validateInput(pr_name, pr_address, pr_city)) {
+                    String projectName = pr_name.getText().toString();
+                    Toast.makeText(getContext(), "Project name: " + projectName, Toast.LENGTH_SHORT).show();
+                    alertDialog.dismiss();
+                } else {
+                    Toast.makeText(getContext(), "not validated", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
 
     }
 
+    private boolean validateInput(EditText pr_name, EditText pr_address, EditText pr_city) {
+
+        String projectName = pr_name.getText().toString();
+        String address = pr_address.getText().toString();
+        String cityName = pr_city.getText().toString();
+
+
+        if (projectName.isEmpty()) {
+            pr_name.setError("project name is required");
+            pr_name.requestFocus();
+            return false;
+        }
+
+        if (address.isEmpty()) {
+            pr_address.setError("Address is required");
+            pr_name.requestFocus();
+            return false;
+        }
+
+        if (cityName.isEmpty()) {
+            pr_city.setError("City name is required");
+            pr_city.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
 
     private void createdata() {
-        String projectname=pr_name.getText().toString();
-        String projectaddress=pr_address.getText().toString();
-        String projectcity=pr_city.getText().toString();
-        String id=databaseProjects.push().getKey();
+        String projectname = pr_name.getText().toString();
+        String projectaddress = pr_address.getText().toString();
+        String projectcity = pr_city.getText().toString();
+        String id = databaseProjects.push().getKey();
 
 
         if (projectname.isEmpty()) {
@@ -131,7 +163,6 @@ public class Home_Fragment extends Fragment {
         if (projectcity.isEmpty()) {
             pr_city.setError("Required!");
             pr_city.requestFocus();
-            return;
         }
 
     }
